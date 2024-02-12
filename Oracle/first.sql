55971cb830f4087ccd363977623a23b883953620
llivawspt <130529573+llivawspt@users.noreply.github.com>
2024-02-12 21:49:41 +0800

    Create first.sql

---
+++ Oracle/first.sql
@@ -0,0 +1,21 @@
+-- 创建一个名为Student的基本表
+create table Student(
+  Sname varchar2(60),
+  Sno varchar2(30),
+  Ssex varchar2(10),
+  Sscore number
+);
+
+-- 创建一个含有列级完整性约束性条件的表
+
+
+
+-- 创建一个含有表级完整性约束条件的表
+
+
+-- 修改已有表的属性或者约束条件
+
+
+-- 创建视图
+
+--
